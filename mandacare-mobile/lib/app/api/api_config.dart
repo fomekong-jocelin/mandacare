af194be87f4789605c1b757c07023071044cdbc7
Jocelin FOMEKONG <jocelin.fomekong@indyli-services.com>
2026-05-30 16:38:11 +0200

    chore: point mobile app to deployed api

--- mandacare-mobile/lib/app/api/api_config.dart
+++ mandacare-mobile/lib/app/api/api_config.dart
@@ -3,6 +3,6 @@ class ApiConfig {
 
   static const baseUrl = String.fromEnvironment(
     'MANDACARE_API_BASE_URL',
-    defaultValue: 'http://127.0.0.1:8080/api/v1',
+    defaultValue: 'http://161.97.181.177:8082/api/v1',
   );
 }
